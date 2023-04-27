7c11da42c6951d44b69e16efa330d5caa3a98f25
Alexandra Lebedzeva <evtuhova15vek44@gmail.com>
2023-04-27 21:08:12 +0300

    add authentication

---
+++ demo/src/main/java/com/appSecurity/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.appSecurity.config;
+
+
+import com.appSecurity.security.AuthProviderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthProviderImpl authProvider;
+
+    @Autowired
+    public SecurityConfig(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    //настраивает аутентификацию
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authProvider);
+    }
+}
